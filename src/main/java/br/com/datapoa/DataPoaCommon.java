a03cd0a8d0b92aaf5390d27ba772d42dde12d462
Cristian Oliveira <contato@cristianoliveira.com.br>
2015-02-22 23:13:03 -0300

    Continuos integration Travis Ci.

--- src/main/java/br/com/datapoa/DataPoaCommon.java
+++ src/main/java/br/com/datapoa/DataPoaCommon.java
@@ -1,24 +1,22 @@
 package br.com.datapoa;
 
 public class DataPoaCommon {
-	
-	private static IDataProvider provider;
-	
-	public static void setProvider(IDataProvider pProvider)
-	{
-		provider = pProvider;
-	}
-	
-	public static IDataProvider getProvider()
-	{
-		if(provider == null)
-		{
-			return new DataPoaProvider();
-		} 
-		else
-		{
-			return provider;	
-		}
-	}
+    
+    private static IDataProvider provider;
+    
+    public static void setProvider(IDataProvider pProvider)
+    {
+        provider = pProvider;
+    }
+    
+    public static IDataProvider getProvider()
+    {
+        if(provider == null)
+        {
+            provider = new DataPoaProvider();
+        }
+       
+        return provider;
+    }
 
 }
