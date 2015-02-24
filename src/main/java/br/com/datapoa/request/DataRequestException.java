eabe678dfcb04d86e3384b485d70f6fbfa144424
Cristian Oliveira <contato@cristianoliveira.com.br>
2015-02-24 14:09:13 -0300

    DataRequestException

---
+++ src/main/java/br/com/datapoa/request/DataRequestException.java
@@ -0,0 +1,29 @@
+package br.com.datapoa.request;
+
+import java.io.IOException;
+
+public class DataRequestException extends IOException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static int WHEN_REQUESTING_DATA = 1;
+	public static int WHEN_PARSING_DATA = 2;
+	public static int WHEN_POSTING_DATA = 3;
+	public static int WHEN_PARSING_RESOURCE = 4;
+	
+	private int pointOfError;
+	
+	DataRequestException(Exception innerException, Integer point){
+		super(innerException);
+		this.pointOfError = point;
+	}
+	
+	public int getPointOfError()
+	{
+		return this.pointOfError;
+	}
+
+}
